39be585acc17b78ee4458b3363f512fec13a55af
Sinard2-Minion <kalenkow56_dk@mail.ru>
2026-06-30 23:39:17 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_file, load_dotenv
+
+# Загружаем переменные из .env файла (если запускаем локально)
+load_dotenv()
+
+TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ROLE_ID = int(os.getenv("ADMIN_ROLE_ID", 0))
+REVIEWS_CHANNEL_ID = int(os.getenv("REVIEWS_CHANNEL_ID", 0))
+WICK_LOGS_CHANNEL_ID = int(os.getenv("WICK_LOGS_CHANNEL_ID", 0))
+AUDIO_LOGS_CHANNEL_ID = int(os.getenv("AUDIO_LOGS_CHANNEL_ID", 0))
